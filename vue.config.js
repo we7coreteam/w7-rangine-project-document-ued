8ed30c3e67f5da0c5248a3bc62ecd4f993633251
gaojianye <369345486@qq.com>
2019-08-26 10:32:52 +0800

    client

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,23 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/admin': {
-					target: 'http://172.16.1.13:98/',
-					// secure: false,
-					changeOrigin: true,
-					headers: {
-						'User-Agent': "we7test-develop",
-						host: 'https://wiki.w7.cc/'
-					},
-			}
+				target: 'http://172.16.1.13:98/',
+				// secure: false,
+				changeOrigin: true,
+				headers: {
+					'User-Agent': "we7test-develop",
+					host: 'https://wiki.w7.cc/'
+				},
+			},
+			'/client': {
+				target: 'http://172.16.1.13:98/',
+				// secure: false,
+				changeOrigin: true,
+				headers: {
+					'User-Agent': "we7test-develop",
+					host: 'https://wiki.w7.cc/'
+				},
+		}
 		}
 	},
 	chainWebpack: config => {
