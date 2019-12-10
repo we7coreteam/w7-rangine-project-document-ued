dde8a5489fa13abe8f0e5c46266187ed7c4f9d69
RenChao <donknap@gmail.com>
2019-12-10 16:29:06 +0800

    优化登录接口

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
   // base: process.env.BASE_URL,
   routes: [
     {
-      path: '/admin/login',
+      path: '/login',
       name: 'adminLogin',
       component: () => import(/* webpackChunkName: "about" */ './views/admin/login.vue')
     },
