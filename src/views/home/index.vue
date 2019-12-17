3ac5f7a559c6f47e637517a25e9430ec0eb59346
guojunchao <165363911@qq.com>
2019-12-17 20:34:25 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -118,6 +118,7 @@ export default {
         })
     },
     handleNodeClick(obj) {
+      console.log(obj)
       this.changeRoute(obj.id, obj.name)
     },
     changeRoute(id, name, handSelectNode) {
