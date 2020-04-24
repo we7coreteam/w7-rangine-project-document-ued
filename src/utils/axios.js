0f6f99cdd7634ed31de6d030b91ebecada7d22fe
wangyg <743471308@qq.com>
2020-04-24 19:06:59 +0800

    登录

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import router from '../router'
 import {Message} from 'element-ui'
 import qs from 'qs'
 
@@ -18,6 +19,7 @@ instance.interceptors.response.use(response => {
     return response.data;
   } else {
     if (response.data.code == '444') {
+      router.push({name: 'adminLoginPage'});
       Message.closeAll();
       Message.error(response.data.message);
     } else {
